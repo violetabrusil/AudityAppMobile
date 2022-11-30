995082a807f27a02f91a25984c4470b5d071bf9a
violetabrusil <josselyn.brusil@epn.edu.ec>
2022-11-30 18:31:56 -0500

    Integración del microservici Uerplaylistmicroservice

---
+++ Model/PlayList.swift
@@ -0,0 +1,8 @@
+//
+//  PlayList.swift
+//  Audity (iOS)
+//
+//  Created by Violeta on 11/25/22.
+//
+
+import Foundation
---
+++ Service/PlayListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayListViewModel.swift
+//  Audity (iOS)
+//
+//  Created by Violeta on 11/25/22.
+//
+
+import Foundation
